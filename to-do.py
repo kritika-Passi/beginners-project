56dd829f5927a5330ab48a7b6039c3d0d4c701d8
kritika-Passi <herekritikapassi@gmail.com>
2024-03-27 05:35:21 +0530

    indexed the todo list

---
+++ to-do.py
@@ -0,0 +1,25 @@
+from datetime import date
+
+def todo():
+    user_input_arr = []
+    i = 1
+    while True:
+            user_input = input(f"enter task {i}(or press N to end the list)")
+            if user_input.lower() == "n":
+                break
+            user_input_arr.append(user_input)
+            i = i+1
+
+    return user_input_arr
+
+today = date.today()
+day = today.strftime("%B %d, %Y")
+print("Today is : ", day)
+print("\n")
+
+print("Please enter the task below : ")
+final_todo = todo()
+print("TO-DO : ")
+for idx ,items in enumerate(final_todo, start = 1):
+    print(idx,".", items)
+
