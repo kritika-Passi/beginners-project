114f88677ef1b40e5898a3b54d8812ec9e969104
kritika-Passi <herekritikapassi@gmail.com>
2024-03-29 22:49:02 +0530

    defined functions to call for the task

--- to-do.py
+++ to-do.py
@@ -1,7 +1,7 @@
 from datetime import date
 
+user_input_arr = []
 def todo():
-    user_input_arr = []
     i = 1
     while True:
             user_input = input(f"enter task {i}(or press N to end the list)")
@@ -12,26 +12,36 @@ def todo():
 
     return user_input_arr
 
+def remove():
+     
+def done():
+def edit():
+     
+
 today = date.today()
 day = today.strftime("%B %d, %Y")
 print("Today is : ", day)
 print("\n")
 
-
-user_wish = input("""What would you like to do :\n
+while True:
+    print("""What would you like to do :\n
       Press T for adding a task.
       Press R for removing a task.
       Press D for marking a task Done.
       Press E for editing a task.
-      Press Q to quit""")
-
-while (user_wish.lower() != "Q"):
+      Press Q to quit\n""")
+    user_wish = input("")
     if user_wish == "":
          print("please first type something")
-    elif user_wish.lower() == "T":
+    else:
+        if user_wish.lower() == "T":
           final_todo = todo()
-    elif user_wish.lower() == "R":
-        rm_task = input(print("Please mention the task you would like to delete"))
+        elif user_wish.lower() == "R":
+            rm_task = remove()
+        elif user_wish.lower() == "D":
+             done_task = done()
+        elif user_wish.lower() == "E":
+             edit_task = edit()
             
 
 
